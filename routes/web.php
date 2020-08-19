5fd694d66e57cbcdd6cfaa7aaeabcec46d62fbaf
Tatani-Herman <64686426+Tatani-Herman@users.noreply.github.com>
2020-08-19 15:38:26 +0100

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -21,4 +21,4 @@ Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
 
-Route::get('/profiles/{user}', 'ProfileController@show')->name('profiles.show');
+Route::get('/profiles/{username}', 'ProfileController@show')->name('profiles.show');
