2aacd763ce9ccf5a711fb1c1597eb542a3c47903
Tatani-Herman <64686426+Tatani-Herman@users.noreply.github.com>
2020-08-19 21:05:27 +0100

    Update ProfileController.php

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -2,12 +2,15 @@
 
 namespace App\Http\Controllers;
 
+use App\User;
 use Illuminate\Http\Request;
 
 class ProfileController extends Controller
 {
     public function show($username)
     {
+        $user=User::find($username);
+        dd($user);
         return view('profiles.show');
     }
 }
