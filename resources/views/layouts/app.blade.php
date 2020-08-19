37f675935f4062c611aee2df61708c6e89a1dc0f
Tatani-Herman <64686426+Tatani-Herman@users.noreply.github.com>
2020-08-19 16:06:32 +0100

    Update app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -24,7 +24,8 @@
         <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
             <div class="container">
                 <a class="navbar-brand" href="{{ url('/') }}">
-                    StarGram
+                <img src="{{ asset('svg/instagram.svg') }}" width="35px" class="pr-3" style="border-right: solid 1px #333;">
+                <span class="pl-3">StarGram</span>
                 </a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
