44efdf1cf445eb74ec92066f7f37e8208bbe89b3
Tatani-Herman <64686426+Tatani-Herman@users.noreply.github.com>
2020-08-19 16:44:09 +0100

    Update show.blade.php

--- resources/views/profiles/show.blade.php
+++ resources/views/profiles/show.blade.php
@@ -2,8 +2,27 @@
 
 @section('content')
 <div class="container">
-    <div class="row">
-        
+    <div class="row mt-4">
+       <div class="col-4 text-center">
+       <img src="https://www.secret-source.eu/wp-content/uploads/2017/11/Laravel-logo.jpg" class="rounded-circle">
+       </div>
+
+       <div class="col-8">
+      <div class="d-flex">
+      <div class="h4 mr-3 pt-2">Laravel</div>
+       <button class="btn btn-sm btn-primary">S'abonner</button>
+      </div>
+      <div class="d-flex mt-3">
+        <div class="mr-3"><strong>25</strong> publications</div>
+        <div class="mr-3"><strong>100</strong> abonnés</div>
+        <div class="mr-3"><strong>20</strong> abonnements</div>
+      </div>
+      <div class="mt-3">
+      <div>framework Laravel</div>
+      <div>Apprenez à utiliser Laravel</div>
+      <a href="#">Laravel.com</a>
+      </div>
+       </div>
     </div>
 </div>
 @endsection
