ce26258b1f8f980801dfd29fe0f22c1417fc1c1c
Tatani-Herman <64686426+Tatani-Herman@users.noreply.github.com>
2020-08-20 03:00:27 +0100

    Update Post.php

--- app/Post.php
+++ app/Post.php
@@ -5,7 +5,9 @@ namespace App;
 use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
-{
+{   
+    protected $guarded = [];
+    
     public function user()
     {
         return $this->belongsTo('App\User');
