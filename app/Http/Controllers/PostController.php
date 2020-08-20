0a764c336a9932d6cdf358cb4dbfa1cc096e5db7
Tatani-Herman <64686426+Tatani-Herman@users.noreply.github.com>
2020-08-20 02:57:52 +0100

    Update PostController.php

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -18,9 +18,16 @@ class PostController extends Controller
 
     public function store()
     {
-        request()->validate([
+        $data = request()->validate([
          'caption' => ['required', 'string'],
          'image' => ['required', 'image']
+        ]);
+
+        $imagePath = request('image')->store('uploads', 'public');
+
+        auth()->user()->posts()->create([
+            'caption' => $data['caption'],
+            'image' => $imagePath       // on ne peut pas mettre $data['image'] car c est un chemin temporaire et pas le chemin effectif de l'image
         ]);
     }
 }
