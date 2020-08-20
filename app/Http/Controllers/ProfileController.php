ef6b9dda2c1816e3c4727c6afc8a3c796f9916cc
Seta-art <tollystephane@gmail.com>
2020-08-20 16:55:04 +0100

    Adding A policy to profile editing for profile owner Only

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -14,11 +14,13 @@ class ProfileController extends Controller
 
     public function edit(User $user)
     {
+        $this->authorize('update', $user->profile);
         return view('profiles.edit', compact('user'));
     }
 
     public function update(User $user)
     {
+        $this->authorize('update', $user->profile);
         $data = request()->validate([
             'title' => 'required',
             'description' => 'required',
