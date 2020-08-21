90e876c1511c53bd5c1f83a47211ee77795378f8
ngan-joseph <64711575+ngan-joseph@users.noreply.github.com>
2020-08-21 17:44:51 +0100

    Mise en Place du controlleur Follow

--- public/js/app.js
+++ public/js/app.js
@@ -1912,6 +1912,8 @@ __webpack_require__.r(__webpack_exports__);
 //
 //
 //
+//
+//
 /* harmony default export */ __webpack_exports__["default"] = ({});
 
 /***/ }),
@@ -37498,9 +37500,20 @@ var render = function() {
   var _vm = this
   var _h = _vm.$createElement
   var _c = _vm._self._c || _h
-  return _c("div")
+  return _vm._m(0)
 }
-var staticRenderFns = []
+var staticRenderFns = [
+  function() {
+    var _vm = this
+    var _h = _vm.$createElement
+    var _c = _vm._self._c || _h
+    return _c("div", [
+      _c("button", { staticClass: "btn btn-sm btn-primary" }, [
+        _vm._v("S'abonner")
+      ])
+    ])
+  }
+]
 render._withStripped = true
 
 
