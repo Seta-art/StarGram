2c8050495f940afb25e570913b95141bf5a9067d
ngan-joseph <64711575+ngan-joseph@users.noreply.github.com>
2020-08-21 18:15:16 +0100

    Update FollowController.php

--- app/Http/Controllers/FollowController.php
+++ app/Http/Controllers/FollowController.php
@@ -6,8 +6,12 @@ use Illuminate\Http\Request;
 
 class FollowController extends Controller
 {
+	public function __construct()
+	{
+		 $this->middleware('auth');
+	}
     public function store($profile)
     {
-    	return $profile;
+    	return auth()->user()->following()->toggle($profile);
     }
 }
