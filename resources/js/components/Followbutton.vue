e02cb05130259e8108ab331a374810d8681ae0bd
ngan-joseph <64711575+ngan-joseph@users.noreply.github.com>
2020-08-21 18:00:38 +0100

    Update Followbutton.vue

--- resources/js/components/Followbutton.vue
+++ resources/js/components/Followbutton.vue
@@ -1,11 +1,19 @@
 <template>
     <div>
-    <button class="btn btn-sm btn-primary">S'abonner</button>
+    <button class="btn btn-sm btn-primary" @click='followProfile'>S'abonner</button>
     </div>
 </template>
 
 <script>
     export default {
      
+     methods: {
+     	followProfile(){
+     	axios.post('/follows/1')
+     	     .then(response => {
+     	          console.log(response.data);
+     	     })
+     	}
+     }
     }
 </script>
