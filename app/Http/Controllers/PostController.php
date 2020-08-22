0c147a5ff3ce148c859408f6cafa5b103bcf1f43
ngan-joseph <64711575+ngan-joseph@users.noreply.github.com>
2020-08-22 11:34:17 +0100

    Update PostController.php

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -14,6 +14,13 @@ class PostController extends Controller
         $this->middleware('auth');
     }
 
+    public function index()
+    {
+        $users= auth()->user()->following->pluck('user_id');
+        $posts= Post::whereIn('user_id', $users)->latest()->get();
+        return view('posts.index', compact('post'));
+    }
+
     public function create()
     {
         return view('posts.create');
