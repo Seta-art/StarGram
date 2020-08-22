3c9d45c5e14a658f5f1faa04f4afbed4a7f2b0ef
ngan-joseph <64711575+ngan-joseph@users.noreply.github.com>
2020-08-22 13:51:26 +0100

    Systeme de Cache

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Cache;
 use Intervention\Image\Facades\Image;
 
 class ProfileController extends Controller
@@ -13,9 +14,15 @@ class ProfileController extends Controller
     {
         $follows= (auth()->user()) ? auth()->user()->following->contains($users->profile->id) : false;
 
-        $postsCount= $user->posts->count();
-        $followersCount= $user->profile->followers->count();
-        $followingCount= $user->following->count()
+        $postsCount= Cache::remember('posts.count' . $user->id, now()->addSeconds(30), function() use ($user){
+            return $user->posts->count();
+        });
+         $followersCount= Cache::remember('followers.count' . $user->id, now()->addSeconds(30), function() use ($user){
+            return $user->profile->followers->count();
+        });
+          $followingCount= Cache::remember('following.count' . $user->id, now()->addSeconds(30), function() use ($user){
+            return $user->following->count();
+        }); 
         return view('profiles.show', compact('user','follows','postsCount', 'followersCount', 'followingCount'));
     }
 
