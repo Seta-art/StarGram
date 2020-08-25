06f2ff860ebb31a4defcd556052cf042a9bd2eb2
Tatani-Herman <64686426+Tatani-Herman@users.noreply.github.com>
2020-08-25 10:33:30 +0100

    Update ProfileController.php

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -8,33 +8,37 @@ use Illuminate\Support\Facades\Cache;
 use Intervention\Image\Facades\Image;
 
 class ProfileController extends Controller
-
 {
     public function show(User $user)
     {
-        $follows= (auth()->user()) ? auth()->user()->following->contains($users->profile->id) : false;
+        $follows = (auth()->user()) ? auth()->user()->following->contains($user->profile->id) : false;
 
-        $postsCount= Cache::remember('posts.count' . $user->id, now()->addSeconds(30), function() use ($user){
+        $postsCount = Cache::remember('posts.count' . $user->id, now()->addSeconds(30), function () use ($user) {
             return $user->posts->count();
         });
-         $followersCount= Cache::remember('followers.count' . $user->id, now()->addSeconds(30), function() use ($user){
+
+        $followersCount = Cache::remember('followers.count' . $user->id, now()->addSeconds(30), function () use ($user) {
             return $user->profile->followers->count();
         });
-          $followingCount= Cache::remember('following.count' . $user->id, now()->addSeconds(30), function() use ($user){
+
+        $followingCount = Cache::remember('following.count' . $user->id, now()->addSeconds(30), function () use ($user) {
             return $user->following->count();
-        }); 
-        return view('profiles.show', compact('user','follows','postsCount', 'followersCount', 'followingCount'));
+        });
+
+        return view('profiles.show', compact('user', 'follows', 'postsCount', 'followersCount', 'followingCount'));
     }
 
     public function edit(User $user)
     {
         $this->authorize('update', $user->profile);
+
         return view('profiles.edit', compact('user'));
     }
 
     public function update(User $user)
     {
         $this->authorize('update', $user->profile);
+
         $data = request()->validate([
             'title' => 'required',
             'description' => 'required',
@@ -44,22 +48,17 @@ class ProfileController extends Controller
 
         if (request('image')) {
             $imagePath = request('image')->store('avatars', 'public');
-            //$image=Image::make(public_path("/storage/[$imagePath]"))->fit(800,800);
-            $image=Image::make(request('image')->getRealPath())->fit(800,800);
-            //$image->save();
-            $image->stream();
-            
+            $image = Image::make(public_path("/storage/{$imagePath}"))->fit(800, 800);
+            $image->save();
+
             auth()->user()->profile->update(array_merge(
                 $data,
-                ['image'=>$imagePath]
+                ['image' => $imagePath]
             ));
-        }
-        else {
+        } else {
             auth()->user()->profile->update($data);
         }
 
-        
-
-        return redirect()->route('profiles.show', ['user' =>$user]);
+        return redirect()->route('profiles.show', ['user' => $user]);
     }
 }
