46a2f911d59ea989162c3d5bd37f2f572ee550de
Seta-art <tollystephane@gmail.com>
2020-09-05 12:33:36 +0100

    Search Bar on Dashboard

---
+++ resources/views/partials/search.blade.php
@@ -0,0 +1,6 @@
+<form action="" method="get">
+    <div class="form-group">
+    <label for="box">Search a  Profile</label>
+    <input type="text" name="q" class="form-control" id="box">
+    </div>
+</form>
